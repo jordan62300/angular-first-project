8be20cb7612a0d65f0113a2eefb7bc49be5c7d1c
jordan62300 <jordanfievetpro@outlook.fr>
2019-05-16 14:36:52 +0200

    ajout de la fonction de mise a jour de la date

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
  
   isAuth: boolean = false;
+  lastUpdate = new Promise((resolve, reject)=> {
+    const date = new Date();
+    setTimeout( 
+      ()=> 
+      {
+      resolve(date);
+    },2000
+    );
+  });
 
   appareils = [
     {
