17a8745afaeecf29395e4009e88140eb8b34bbe3
jordan62300 <jordanfievetpro@outlook.fr>
2019-06-28 16:10:14 +0200

    ajout de l'observable comptant les secondes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,39 @@
 import { Component,OnInit } from '@angular/core';
 import { AppareilService } from './services/appareil.Service';
+import { Observable, Subject } from 'rxjs';
+import { interval } from 'rxjs';
+import { map, filter, scan } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
- 
+
+
+export class AppComponent implements OnInit{
   
+  secondes: number;
+
+  ngOnInit() {
+    const counter = interval(1000);
+
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('an error occured ! : ' + error)
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    )
+
+  }
+
+
+
  
 
 }
